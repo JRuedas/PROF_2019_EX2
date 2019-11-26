63cc28ce39d76d2ac17ee725b33ed1e48bdb7961
Jonatan <jonatanruedasmora@gmail.com>
2019-11-26 15:40:39 +0100

    First commit

---
+++ src/main/java/es/upm/fi/PROF_2019_EX2/App.java
@@ -0,0 +1,13 @@
+package es.upm.fi.PROF_2019_EX2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
